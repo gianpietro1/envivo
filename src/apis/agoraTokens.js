b31ecabd9e77ead1b14f449a0d645226967eba6e
lavado <glavado@whitestack.com>
2020-04-22 18:00:02 -0500

    db update

--- src/apis/agoraTokens.js
+++ src/apis/agoraTokens.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV.REACT_APP_APIHOST_AGORA,
+  // baseURL: process.env.NODE_ENV.REACT_APP_APIHOST_AGORA,
+  baseURL: "https://envivo-server.herokuapp.com",
 });
--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV.REACT_APP_APIHOST_JSON,
+  //baseURL: process.env.NODE_ENV.REACT_APP_APIHOST_JSON,
+  baseURL: "https://envivo-api.herokuapp.com",
 });
